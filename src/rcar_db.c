7f4d20fcf1035fbad66e336555b303d86996f0d5
Roman Borisov <rborisoff@gmail.com>
2014-11-26 16:34:49 +0300

    db development

--- src/rcar_db.c
+++ src/rcar_db.c
@@ -31,3 +31,85 @@ int db_create()
 
     return 1;
 }
+
+int db_create_tables()
+{
+    MYSQL *con = mysql_init(NULL);
+
+    if (con == NULL)
+    {
+        fprintf(stderr, "%s\n", mysql_error(con));
+        return 0;
+    }
+
+    if (mysql_real_connect(con, "localhost", "pi", "raspberry", "rcardb", 0, 
+                NULL, 0) == NULL)
+       goto error; 
+    if (mysql_query(con, "DROP TABLE IF EXISTS Songs"))
+        goto error;
+    if (mysql_query(con, "DROP TABLE IF EXISTS Albums"))
+        goto error;
+    if (mysql_query(con, "DROP TABLE IF EXISTS Artists"))
+        goto error;
+    if (mysql_query(con, "CREATE TABLE Songs(Id INT KEY AUTO_INCREMENT,\
+        Name TEXT, Artist TEXT, Album TEXT, Listened INT, Rating INT)"))
+        goto error;
+    if (mysql_query(con, "CREATE TABLE Albums(Id INT KEY AUTO_INCREMENT,\
+        Name TEXT, Artist TEXT, Art TEXT, Listened INT, Rating INT)"))
+            goto error;
+    if (mysql_query(con, "CREATE TABLE Artists(Id INT KEY AUTO_INCREMENT,\
+        Name TEXT, Art TEXT, Listened INT, Rating INT)"))
+        goto error;
+
+    mysql_close(con);
+    return 1;
+
+error:
+    fprintf(stderr, "%s\n", mysql_error(con));
+    mysql_close(con);
+    return 0;
+}
+
+int db_put_listen(char* name, char* artist, char* album)
+{
+    MYSQL *con = mysql_init(NULL);
+    char query[128];
+    int status = 0;
+
+    if (con == NULL)
+    {
+        fprintf(stderr, "%s\n", mysql_error(con));
+        return 0;
+    }
+
+    if (mysql_real_connect(con, "localhost", "pi", "raspberry", "rcardb", 0,
+                NULL, 0) == NULL)
+        goto error;
+
+    sprintf(query, "SELECT Artists FROM Songs WHERE Name=%s", name);
+    if (mysql_query(con, query))
+        goto error;
+
+    do {
+        MYSQL_RES *result = mysql_store_result(con);
+        if (result == NULL)
+            goto error;
+        MYSQL_ROW row = mysql_fetch_row(result);
+        printf("%s\n", row[0]);
+        //TODO: if there are Song and Artist: increase Listened
+        //and check Album
+        //...
+        mysql_free_result(result);
+        status = mysql_next_result(con);
+        if (status > 0)
+            goto error;
+    } while (status == 0);
+
+    mysql_close(con);
+    return 1;
+
+error:
+    fprintf(stderr, "%s\n", mysql_error(con));
+    mysql_close(con);
+    return 0;
+}
--- src/rcar_db.h
+++ src/rcar_db.h
@@ -2,5 +2,6 @@
 #define __RCAR_DB_H__
 
 int db_create();
+int db_create_tables();
 
 #endif //__RCAR_DB_H__
